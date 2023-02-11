20a0530cc71d88582eb313b93e4df8d17e9dad90
Csf32 <c.souzaf@hotmail.com>
2023-02-11 18:29:47 -0300

    Create materia ltable/interface/service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -17,6 +18,7 @@ import { AppComponent } from './app.component';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatSlideToggleModule,
+    HttpClientModule
 
   ],
   providers: [],
--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Course } from '../model/course';
+import { CoursesService } from '../services/courses.service';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.scss']
 })
-export class CoursesComponent {
+export class CoursesComponent implements OnInit{
+
+  courses: Course[] = [
+    ];
+
+  displayedColumns = ["name", "category"];
+
+  constructor(private coursesService: CoursesService) {
+
+    this.courses = this.coursesService.list();
+  }
+  ngOnInit(): void {
+    
+    this.courses = this.coursesService.list()
+  }
 
 }
